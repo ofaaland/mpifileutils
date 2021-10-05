937fdefcaa545d2496b4f704548bf59fcf088ccc
Olaf Faaland <faaland1@llnl.gov>
2021-10-05 16:42:20 -0700

    debug: report when copying an attribute

--- src/common/mfu_flist_copy.c
+++ src/common/mfu_flist_copy.c
@@ -358,8 +358,12 @@ static int mfu_copy_xattrs(
                         strcmp(name,"dummy") == 0)
                 {
                     skip_xattr = 1;
+                    MFU_LOG(MFU_LOG_WARN, "Skipping aribute name=%s on `%s'",
+                            name, src_path);
                 } else {
                     skip_xattr = 0;
+                    MFU_LOG(MFU_LOG_WARN, "Attempting to copy aribute name=%s on `%s'",
+                            name, src_path);
                 }
             }
 
